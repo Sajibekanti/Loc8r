821f2cf698bf72cdf5f0e976f355c971959aa628
Manash Kumar Mandal <manashmndl@gmail.com>
2016-07-02 01:54:45 +0600

    fixed views

---
+++ app_server/models/db.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Manash on 7/1/2016.
+ */
+
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
